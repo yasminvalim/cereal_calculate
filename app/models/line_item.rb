f674aa74b12c632e830f372d40454fe66d8f3865
Victor Feijo <victor.feijoa@gmail.com>
2023-05-23 12:20:02 -0700

    General feedbacks

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,6 +1,8 @@
 class LineItem < ApplicationRecord
   has_many :carts
 
+  # Question: Any specific reason to use a enum instead
+  # of a simply using a String?
   enum collection: { default: 0, best_seller: 1, keto: 2 }
   scope :discountable, -> { where.not(collection: 2)}
 end
